c5100b1cfd663bd2826b3b2886ccfd8798ec0095
Kasper Emil Okkels <kasperemil38@gmail.com>
2015-11-12 16:34:17 +0100

    Skrevet midlertidig fleet, rente setter. +!

--- 24_del3/src/game/Fleet.java
+++ 24_del3/src/game/Fleet.java
@@ -1,9 +1,32 @@
 package game;
 
 public class Fleet extends Ownable {
-	
-private int RENT_1 = 500;
-private int RENT_2 = 1000;
-private int RENT_3 = 2000;
-private int RENT_4 = 4000;
+    private int RENT_1 = 500;
+    private int RENT_2 = 1000;
+    private int RENT_3 = 2000;
+    private int RENT_4 = 4000;
+    public Fleet() {
+price=4000;
+
+}
+    public int setRent(int nr){
+switch (nr){
+case 1:
+    rent=RENT_1;
+    break;
+case 2:
+    rent=RENT_2;
+    break;
+case 3:
+    rent=RENT_3;
+    break;
+case 4:
+    rent=RENT_4;
+    break;
+            
+        
+        
+    }
+return rent;
+    }
 }
