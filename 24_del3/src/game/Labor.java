5ceb613d90230c6e27681db56e2938505824caf5
Kasper Emil Okkels <kasperemil38@gmail.com>
2015-11-13 10:07:27 +0100

    Skrevet Labor, skrevet javadoc til Fleet og Labor.

--- 24_del3/src/game/Labor.java
+++ 24_del3/src/game/Labor.java
@@ -2,4 +2,17 @@ package game;
 
 public class Labor extends Ownable {
 private int baserent;
+public Labor(){
+    super.price=2500;
+}
+//rente beregning kan blive gjort her, jeg er bare bange for at den bliver fast sat.
+/**
+ * Used when landing on an owned Labor camp.
+ * @param dice
+ * @return
+ */
+public int payrent(int dice){
+    rent=100*dice;
+    return rent;
+}
 }
