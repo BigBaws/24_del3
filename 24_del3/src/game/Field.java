a812413a13283da3a5fdb291fccf9a7ee8d417b9
Kasper Emil Okkels <kasperemil38@gmail.com>
2015-11-14 14:34:07 +0100

    Spillere tilføjes nu de felter de lander på. Funktion til at fjerne dem mangler.

--- 24_del3/src/game/Field.java
+++ 24_del3/src/game/Field.java
@@ -2,7 +2,34 @@ package game;
 
 public class Field {
     protected String name;
+    protected Player[] players = new Player[6];
+    /**
+     * tilføjer spilleren til feltet. indtager spillerens navn.
+     * @param player
+     */
 	public void landOnField(Player player) {
+		switch (players.length){
+		    case 0:
+		        players[0]=player;
+		    case 1:
+		        players[1]=player;
+		        break;
+		    case 2:
+                players[2]=player;
+                break;
+		    case 3:
+                players[3]=player;
+                break;
+		    case 4:
+                players[4]=player;
+                break;
+		    case 5:
+                players[5]=player;
+                break;
+            default:
+                System.out.print("FEJL");
+                break;
+		}
 		
 	}
 }
