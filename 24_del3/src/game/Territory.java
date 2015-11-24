3b9603dce9f240dee08cb0dbe9730de810e05a6a
Anthony <Anthony@192.168.1.38>
2015-11-24 18:56:54 +0100

    Territory er blevet opdateret

--- 24_del3/src/game/Territory.java
+++ 24_del3/src/game/Territory.java
@@ -2,14 +2,29 @@ package game;
 
 public class Territory extends Ownable {
     
-    public Territory(String name, int fieldprice, int fieldrent){
-        super.rent=fieldrent;
-        super.price=fieldprice;
-        super.owned=false;
-        super.name=name;
+//  protected Player player;
+    protected String fieldname;
+//  protected int fieldprice;
+    private int fieldrent;
+
+    public Territory(String name, int fieldrent, int fieldvalue) {
+        super(name);
+        this.fieldrent = fieldrent;
+        super.fieldprice = fieldvalue;
+        super.fieldowned=false;
     }
+
     @Override
-    public int getRent(){
-        return rent;
-}
+    public int getRent() {
+        return fieldrent;
+    }
+    
+    public String getName() {
+        return fieldname;
+    }
+
+    @Override
+    public int getPrice() {
+        return fieldprice;
+    }
 }
