7d1777c95f1e12c10b7f3fab8732029a6aea9e12
Anthony <Anthony@192.168.1.38>
2015-11-24 18:49:35 +0100

    Dice klassen lavet

--- 24_del3/src/game/Dice.java
+++ 24_del3/src/game/Dice.java
@@ -1,18 +1,53 @@
 package game;
 
-public class Dice 
-	{
+/**
+ * Dice - Terninger
+ * @author Gruppe 24
+ * @version 12/11-15
+ **/
+
+public class Dice {
+    private int value1, value2;
+    
     /**
-     * terningen aendres til en ny tilfaeldig vaerdi, returnere vaerdien af terningen.
-     * @return
+     * Creating the dices and calling the roll() method, to set dices value to a random,
+     * even though they arent used yet.
      */
-		public int roll()
-		{
-			int resultat = ((int) (Math.random()*6)+1);
-			return resultat;
-			
-		}
-		
-	}
-
-
+    public Dice(){
+        roll();
+    }
+    
+    /**
+     * roll() method is generating two random numbers [1-6]
+     * and setting a new value1 and value2
+     */
+    public void roll(){
+        value1 = (int)(Math.random()*6)+1;
+        value2 = (int)(Math.random()*6)+1;
+    }
+    
+    /**
+     * This will return the number stored in value1
+     * @return int value1
+     */
+    public int getDice1() {
+        return value1;
+    }
+    
+    /**
+     * This will return the number stored in value2
+     * @return int value2
+     */
+    public int getDice2() {
+        return value2;
+    }
+    
+    /**
+     * The method getSum() is returning the sum of value1 and value2
+     * @return value1 + value2
+     */
+    public int getSum(){
+        return value1 + value2;
+    }
+    
+}
