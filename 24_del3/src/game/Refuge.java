dd24473e27fe90bff9a2db1b4c83aa768ac1a8b0
Anthony <Anthony@192.168.1.38>
2015-11-24 18:55:45 +0100

    Refuge oprettet

--- 24_del3/src/game/Refuge.java
+++ 24_del3/src/game/Refuge.java
@@ -1,13 +1,37 @@
 package game;
 
-public class Refuge extends Field{
-private int bonus;
-public Refuge(String name, int bonus){
-    super.name=name;
-    this.bonus=bonus;
-}
-public void landOnField(Player player){
-player.setScore(bonus);    
-}
+import desktop_resources.GUI;
 
+public class Refuge extends Field {
+    
+    protected String fieldname;
+    protected int bonus;
+    
+    public Refuge(String name, int bonus){
+        super(name);
+        this.bonus = bonus;
+    }
+    
+    public void landOnField(Player player){
+        GUI.displayChanceCard("You will get "+bonus);
+        player.giveMoney(bonus);
+        GUI.setBalance(player.getName(), player.getMoney());
+    }
+
+    @Override
+    public int getRent() {
+        return bonus;
+    }
+
+    @Override
+    public String getName() {
+        return fieldname;
+    }
+
+    @Override
+    public int getPrice() {
+        return 0;
+    }
+
+    
 }
