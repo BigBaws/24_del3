7f78de950a4710821cb76788b34a3f5bbdbc5b6f
Anthony <Anthony@192.168.1.38>
2015-11-24 18:52:26 +0100

    Game klassen er oprettet

--- 24_del3/src/game/Game.java
+++ 24_del3/src/game/Game.java
@@ -3,52 +3,63 @@ package game;
 import desktop_resources.GUI;
 
 public class Game {
-
-
-	private static Dice[] dice = { new Dice(), new Dice() };
-	
-	public static void main(String[] args) {
-		new Game();
-	}
-	
-	public Game() {
-		// Create Fields, Both GUI and game logic fields.
-        Fields Field = new Fields();
-		
+    
+    private static Player[] players;
+    private static Field[] fields;   
+    
+    public static void main(String[] args) {
+        new Game();
+    }
+    
+    public Game() {
+        
+        // Create Fields
+        fields = Field.createFields();
+        
+        // Load the Dices
+        Dice Dice = new Dice();
+        
         // How many Players?
-        String numberOfPlayers = GUI.getUserSelection("How many players wish to participate?", "2 Players", "3 Players", "4 Players", "5 Players", "6 Players");
+        String NumberofPlayers = GUI.getUserSelection("", "2 Players", "3 Players", "4 Players", "5 Players", "6 Players");
         
         // Create Player 1,2,3,4,5,6
-        switch(numberOfPlayers){
-            case "6 Players":
-                String playerinput;
-              playerinput= GUI.getUserString("Player 6 type your name");
-                Player Player6 = new Player(playerinput);
-            case "5 Players":
-                playerinput= GUI.getUserString("Player 5 type your name");
-                Player Player5 = new Player(playerinput);
-            case "4 Players":
-                playerinput= GUI.getUserString("Player 4 type your name");
-                Player Player4 = new Player(playerinput);
-            case "3 Players":
-                playerinput= GUI.getUserString("Player 3 type your name");
-                Player Player3 = new Player(playerinput);
-            case "2 Players":
-                playerinput= GUI.getUserString("Player 2 type your name");
-                Player Player2 = new Player(playerinput);
-                playerinput= GUI.getUserString("Player 1 type your name");
-                Player Player1 = new Player(playerinput);
-                // beskriv en tur i en metode. lav while loop der kører indtil der kun er en spiller.
-                // lav en lose metode der fjerner spilleren.
+        switch (NumberofPlayers) {
+            case "6 Players": players = Player.addPlayer(6); break;
+            case "5 Players": players = Player.addPlayer(5); break;
+            case "4 Players": players = Player.addPlayer(4); break;
+            case "3 Players": players = Player.addPlayer(3); break;
+            case "2 Players": players = Player.addPlayer(2); break;
+            default: ;
+        }
+        
+        
+        
+        while (true) {
+            
+            for (int i = 0; i < players.length; i++) {
+                
+                if (players[i].getMoney() < 0) {
+                    continue;
+                }
+                
+                // Roll Dices
+                GUI.getUserButtonPressed("", players[i].getName()+": Roll Dices");
+                Dice.roll();
+                GUI.setDice(Dice.getDice1(), Dice.getDice2());
+                
+                // Move Player
+                players[i].movePlayer(players[i], Dice.getSum());
+                
+                // FIX -1
+                fields[players[i].getPlayerPosition()-1].landOnField(players[i]);
+                
+                
+                
+                
+            }
+            
         }
  
-        }
-        
-        
-        
-        
-        
-        
-
-	}
-
+    }
+    
+}
