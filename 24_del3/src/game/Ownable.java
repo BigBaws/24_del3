aafad927722e32f1d84fff0244353da50d7d72c0
Anthony <Anthony@192.168.1.38>
2015-11-24 18:53:59 +0100

    Game klassen er oprettet

--- 24_del3/src/game/Ownable.java
+++ 24_del3/src/game/Ownable.java
@@ -2,123 +2,75 @@ package game;
 
 import desktop_resources.GUI;
 
-public abstract class Ownable extends Field{
-	
-	protected int price;
-	protected Player player;
-	protected int rent;
-	protected boolean owned;
-	
-	
-	/**
-	 * Bruges naar en spiller lander paa et felt der er ejet af en anden spiller.
-	 * @param player
-	 */
-	
-	public void payRent(Player player){
-		player.setScore(-rent);
-		this.player.setScore(rent);
-		//ejerskab bï¿½r defineres, evt. udnytte dele fra landsOnField().
-	}
-	/**
-	 * Naar spilleren oensker at koebe et ledigt felt. Tager den aktuelle Player.
-	 * @param player
-	 */
-	
-	public void buyField(Player player){
-	    player.setScore(-price);
-	    this.player=player;
-	    player.setOwnedvalue(price);
-	    player.setOwned(this);
-	    owned=true;
-	}
-	/**
-	 * Naar spilleren oensker at saelge et felt. Tager den aktuelle Player.
-	 * @param player
-	 */
-	
-//	public void sellField(Player player){
-//	    player.setScore(price);
-//	    this.player=null;
-//	    player.setOwnedvalue(-price);
-//	    owned=false;
-//	}
-	@Override
-	public void landOnField(Player player){
-	    turn=true;
-	  if (owned=true){
-	      if (this.player!=player)
-	      payRent(player);
-	      String choice=GUI.getUserSelection("What do you wish to do?", "2. Sell fields" , "3. End turn");
-          switch(choice){ 
-          case "2. Sell fields":
-              //evt. tilføj måde så spiller ikke kan sælge hvis han ikke ejer noget.
-             boolean sell=true;
-              while(sell){
-                  player.sellfields();
-                  sell=GUI.getUserLeftButtonPressed("Do you wish to sell more? ", "Continue selling", "Done selling");
-              }
-          case "3. End turn":
-              turn=false;
-              break;
-	  }
-	  }
-	  else{
-	      //evt. restrukturering for at give bredere mulighed, 
-	    //f.eks. at kunne saelge et felt for at faa raad til det nye
-	      while (turn){
-	     String choice=GUI.getUserSelection("What do you wish to do?", "1. Buy ", "2. Sell fields" , "3. End turn");
-	         switch(choice){ 
-	         case "1. Buy ":
-	             buyField(player);
-	         case "2. Sell fields":
-	            boolean sell=true;
-	             while(sell){
-	                 player.sellfields();
-	                 sell=GUI.getUserLeftButtonPressed("Do you wish to sell more? ", "Continue selling", "Done selling");
-	             }
-	         case "3. End turn":
-	             turn=false;
-	             break;
-	          //else statement der slutter tur?
-	      }
-	      }
-	      }
-	  }
-	/**
-	 * tilbagegiver prisen på feltet
-	 * @return
-	 */
-    public int getPrice() {
-            return price;
-        }
-    /**
-     * aedre prisen på feltet, tager den nye pris.
-     * @param price
-     */
-	    public void setPrice(int price) {
-            this.price = price;
-        }
-    /**
-     * henter spillernavnet af ejeren af feltet.
-     * @return
-     */
-	
-    public Player getPlayer() {
-            return player;
-        }
-    /**
-     * tilbagegiver renten på feltet.
-     * @return
-     */
+public abstract class Ownable extends Field {
+    
+public Ownable(String name) {
+        super(name);
+    }
+
+    protected Player fieldowner;
+    protected int fieldprice;
+    protected boolean fieldowned;
+    
     public abstract int getRent();
+    
     /**
-     * tilbagegiver ejerskabs statusen (ejet eller ej)
-     * @return
+     * Bruges nÃ¥r en spiller lander pÃ¥ et felt der er ejet af en anden spiller.
+     * @param player
      */
+    public void payRent(Player player) {
+        int payamount = getRent();
+        player.payMoney(payamount);
+        this.fieldowner.giveMoney(payamount);
+        
+        //ejerskab bï¿½r defineres, evt. udnytte dele fra landsOnField().
+    }
+    
 
-    public boolean isOwned() {
-            return owned;
+    
+    /**
+     * Nï¿½r spilleren ï¿½nsker at kï¿½be et ledigt felt. Tager den aktuelle Player.
+     * @param player
+     */
+    public void buyField(Player player){
+        player.payMoney(fieldprice);
+        fieldowned = true;
+        fieldowner = player;
+        GUI.setOwner(player.getPlayerPosition(), player.getName());
+        GUI.setBalance(player.getName(), player.getMoney());
+    }
+    /**
+     * Nï¿½r spilleren ï¿½nsker at sï¿½lge et felt. Tager den aktuelle Player.
+     * @param player
+     * 
+     */
+    public void sellField(Player player) {
+        player.giveMoney(fieldprice);
+        fieldowned=false;
+        // VI SKAL HAVE EN LISTE MED PLAYERENS FELTER!
+    }
+    
+    @Override
+    public void landOnField(Player player) {
+        GUI.displayChanceCard("You have landed on "+fieldname+"");
+        
+        if (this.fieldowned){
+            payRent(player);
+            GUI.setBalance(player.getName(), player.getMoney());
+            GUI.setBalance(this.fieldowner.getName(), this.fieldowner.getMoney());
+        // Field is not owned    
+        } else {
+            
+            if (player.getMoney() >= fieldprice) {
+                GUI.getUserLeftButtonPressed("Buy "+fieldname+"?", "Buy", "Don't Buy");
+                buyField(player);
+            } else {
+                GUI.getUserLeftButtonPressed("Sell Fields?", "Sell", "Don't Sell");
+            }
+
         }
-
+    }
+    
+    
+    
 }
