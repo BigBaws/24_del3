dd94cb952be0e3bb7046869f1fd0debd645b57d9
Anthony <Anthony@192.168.1.38>
2015-11-24 18:53:30 +0100

    Vi mangler at få terningerne importeret

--- 24_del3/src/game/Labor.java
+++ 24_del3/src/game/Labor.java
@@ -1,24 +1,36 @@
 package game;
 
 public class Labor extends Ownable {
-private int baserent=100;
-public Labor(String name){
-    super.price=2500;
-    super.name=name;
-    super.owned=false;
-}
-//rente beregning kan blive gjort her, jeg er bare bange for at den bliver fast sat.
-/**
- * Used when landing on an owned Labor camp.
- * @param dice
- * @return
- */
-public int payRent(int dice){
-    rent=baserent*dice;
-    return rent;
-}
-@Override
-public int getRent(){
-    return baserent;
-}
+    protected String fieldname;
+    private int baserent;
+     
+    public Labor(String name) {
+        super(name);
+        super.fieldprice=2500;
+        super.fieldowned=false;
+    }
+ 
+    @Override
+    public int getRent() {
+        int fieldrent = 100 * getDiceValue();
+        return fieldrent;
+    }
+    
+    public int getDiceValue() {
+//        Dice dice;
+//        return dice.getSum();
+        
+        return 2;
+    }
+
+    @Override
+    public String getName() {
+        return fieldname;
+    }
+
+    @Override
+    public int getPrice() {
+        return 0;
+    }
+
 }
